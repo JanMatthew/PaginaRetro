023f275de62191b3146d7ec500c9e003e78d3961
Matthew <jmatthewpareja@gmail.com>
2024-12-13 15:29:45 +0100

    Ce Finnie

--- game.js
+++ game.js
@@ -15,19 +15,19 @@ document.addEventListener("DOMContentLoaded", () => {
     let combo = 0;
     let multiplicador = 1;
 
-    // Start the game
+    // Comienza el juego
     playButton.addEventListener("click", () => {
         playButton.style.display = "none";
         gameOverlay.style.display = "flex";
         startCountdown();
     });
-    //Restart the game
+    //Reinicia el juego
     restart.addEventListener("click", () => {
         gameOver.style.display = "none";
         gameOverlay.style.display = "flex"
         startCountdown();
     });
-    // Countdown before game starts
+    // Cuenta atras antes de empezar el juego
     function startCountdown() {
         let counter = 3;
         countdown.textContent = counter;
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 1000);
     }
 
-    // Start the game logic
+    // Empezar la logica del juego
     function startGame() {
         points = 0;
         score.textContent = `Puntos: ${points}`
@@ -55,29 +55,34 @@ document.addEventListener("DOMContentLoaded", () => {
         gameInterval = setInterval(spawnEnemy, 1000); // Spawn enemies every second
     }
 
-    // Spawn enemies
+    // Aparecer enemigos
     function spawnEnemy() {
+
         const enemyContainer = document.createElement("div");
         enemyContainer.classList.add("enemy-container");
 
         const enemy = document.createElement("div");
-
+         
+        //Numero aleatorio para decidir el tipo de enemigo
         let numeroAleatorio = Math.floor(Math.random() * 20);
         let velocity = 0;
         let deathAnimation = "";
 
+        //Enemigo: bicho
         if(numeroAleatorio <10){
-            enemy.classList.add("enemy");
+            enemy.classList.add("bicho");
             enemy.setAttribute("life",3);
             velocity = 5;
             deathAnimation = "bichoDeath"; 
         }
+        //Enemigo: mosca
         else if(numeroAleatorio<17){
             enemy.classList.add("fly")
             enemy.setAttribute("life",1);
             velocity = 7;
             deathAnimation = "flyDeath"; 
         }
+        //Enemigo: tanque
         else{
             enemy.classList.add("tank")
             enemy.setAttribute("life",6);
@@ -92,26 +97,31 @@ document.addEventListener("DOMContentLoaded", () => {
         enemyContainer.appendChild(lifebar);
         enemyContainer.appendChild(enemy);
 
-        // Random horizontal position
+        // Posicion horizontal aleatoria
         enemyContainer.style.left = Math.random() * (window.innerWidth - 50) + "px";
         enemyContainer.style.top = "0px";
 
-        // Add enemy to the game
+        // Añadir enemigo al juego
         game.appendChild(enemyContainer);
 
-        // Animate enemy downward
+        // Animar bajada del enemigo
         const fallInterval = setInterval(() => {
             const currentTop = parseInt(window.getComputedStyle(enemyContainer).top);
+            //Si llega abajo desaparece y se resta una vida
             if (currentTop >= window.innerHeight - 50) {
                 clearInterval(fallInterval);
                 game.removeChild(enemyContainer);
                 loseLife();
-            } else {
+            }
+            //No ha llegado aun, sigue bajando a su velocidad 
+            else {
                 enemyContainer.style.top = currentTop + velocity + "px";
             }
         }, 30);
 
+        //Trigger de disparar al enemigo(Clicarle)
         enemy.addEventListener("click", () => {
+            //Si aun le queda mas de una vida se le resta una y se muestran los puntos y multiplicador
             if(enemy.getAttribute("life")>1){
                 enemy.setAttribute("life",enemy.getAttribute("life")-1)
                 lifebar.src = `img/life/lifebar${enemy.getAttribute("life")}.png`
@@ -120,6 +130,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 points+=(50 * multiplicador);
                 playHitSound("shoot");
                 combo +=1; 
+                //Si el combo es mas de 10 se añade un punto al multiplicador y se reinicia el combo
                 if(combo >= 10){
                     multiplicador+=1;
                     combo = 0;
@@ -127,8 +138,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             }
             else if(enemy.getAttribute("life")==0){
-                //Avoids give points for clicking the corpse
+                //Evitar dar puntos al clicar sobre un enemigo ya muerto
             }
+            //Si le queda una vida se hace la animacion de muerte
             else{
                 playHitSound("death");
                 enemy.setAttribute("life",enemy.getAttribute("life")-1)
@@ -155,6 +167,8 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     }
+
+    //Lanzar el sonido
     function playHitSound(sound) {
         const hitSound = new Audio(`audio/${sound}.mp3`);
         hitSound.play();
@@ -191,26 +205,27 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 1000);
     }
 
-    // Lose a life
+    // Quitar una vida, y reinciar el combo y multiplicador
     function loseLife() {
         lives--;
         livesImg.src = `img/life/lifebar${lives}.png`;
         playHitSound("hit");
         combo = 0;
         multiplicador = 1;
+        //Ya no tiene mas vidas Pierde
         if (lives <= 0) {
             endGame();
         }
     }
 
-    // End the game
+    // Acabar el juego
     function endGame() {
         playHitSound("gameOver");
         clearInterval(gameInterval);
-
+        //Se eliminan todos los enemigos
         const allEnemies = document.querySelectorAll(".enemy-container");
         allEnemies.forEach(enemy => enemy.remove());
-
+        
         pointsScore.textContent = `Puntos: ${points}`
 
         game.style.display = "none";
